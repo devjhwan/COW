fe6318bbddcc016e7cbe4cec0ca97570e6108e0b
jhwan818 <ivusplore@gmail.com>
2025-03-25 17:48:33 +0100

    header change active menu by current page

--- Session5/js/load_includes.js
+++ Session5/js/load_includes.js
@@ -4,35 +4,59 @@ document.observe("dom:loaded", function () {
     onSuccess: function(response) {
       $('header-container').innerHTML = response.responseText;
       check_session();
+      highlightActiveMenu();
     }
   });
+
   new Ajax.Request('../includes/footer.html', {
     method: 'get',
     onSuccess: function(response) {
       $('footer-container').innerHTML = response.responseText;
     }
   });
+});
 
-  function check_session() {
-    new Ajax.Request("../php/check_session.php", {
-      method: "get",
-      onSuccess: function (response) {
-        const data = response.responseJSON;
-  
-        if (data && data.logged_in) {
-          const link = $("login-link");
-          const icon = $("login-icon");
-          const text = $("login-text");
-  
-          if (icon) icon.remove();
-          if (text) text.textContent = data.user_id;
-  
-          link.href = "../php/logout.php";
-        }
-      },
-      onFailure: function () {
-        console.warn("Failed to check session.");
+function check_session() {
+  new Ajax.Request("../php/check_session.php", {
+    method: "get",
+    onSuccess: function (response) {
+      const data = response.responseJSON;
+
+      if (data && data.logged_in) {
+        const link = $("login-link");
+        const icon = $("login-icon");
+        const text = $("login-text");
+
+        if (icon) icon.remove();
+        if (text) text.textContent = data.user_id;
+
+        link.href = "../php/logout.php";
+      }
+    },
+    onFailure: function () {
+      console.warn("Failed to check session.");
+    }
+  });
+}
+
+function highlightActiveMenu() {
+  const path = window.location.pathname;
+  const filename = path.substring(path.lastIndexOf('/') + 1);
+
+  let targetLink = null;
+
+  if (filename !== 'reserve.html') {
+    targetLink = filename;
+  }
+
+  if (targetLink) {
+    const navLinks = $$('#navbarNav .nav-link');
+    navLinks.each(function(link) {
+      if (link.getAttribute('href') === targetLink) {
+        link.up('.nav-item').addClassName('active');
+      } else {
+        link.up('.nav-item').removeClassName('active');
       }
     });
   }
-});
+}
